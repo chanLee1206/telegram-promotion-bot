44f979b5247b5f3e7f4e70b38e622dae0c471c42
root <charmlee1206@gmail.com>
2024-11-11 05:35:18 +0900

    advanced in interface treatment a little but correctly

--- bot/main.py
+++ bot/main.py
@@ -22,10 +22,14 @@ from globals import global_token_arr
 
 last_txn_arr = []
 cur_coin_idx = 0 
+
 user_config = {}
 user_data = {}
 
 curCoinType = ""
+
+front_msg_id = ""
+front_chat_id = ""
 
 async def other_task():
     while True:
@@ -92,19 +96,6 @@ async def run_polling(application):
     while True:
         await asyncio.sleep(0.1)
 
-async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    input_text = update.message.text
-    user_id = update.message.from_user.id
-    if input_text == "ok" and user_id not in user_config:
-        user_config[user_id] = "ok"
-        user_data[user_id] = {"first": input_text}
-        await update.message.reply_text(text="success")
-    if input_text == "yes" and user_id in user_config:
-        text = (
-            f"first text: {user_data[user_id]['first']}\n"
-            f"second text: {input_text}"
-        )
-        await update.message.reply_text(text=text)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -144,17 +135,91 @@ async def handle_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
     
 async def handle_startTrending(update: Update, context: CallbackContext) -> None:
-    chat_id = context.user_data['chat_id']
-    message_id = context.user_data['message_id']
+    global front_msg_id, front_chat_id
+    
+    front_chat_id = context.user_data['chat_id']
+    front_msg_id = context.user_data['message_id']
+    
+    user_config['isStart'] = True
         
     await context.bot.edit_message_text(
-        chat_id=chat_id,
-        message_id=message_id,
+        chat_id=front_chat_id,
+        message_id=front_msg_id,
         text=f"❔ Send me the token's Contract Address or Pair Address or the Launchpad/Presale Url: \n\n Supported Chains: BASE, BSC, ETH, MANTA, POL, SOL, SUI, TRX"
     )
 
+async def handle_coinConfirmation(update: Update, context: CallbackContext) -> None:
+    global front_msg_id, front_chat_id
+
+    text=f"Selected Token: \n\n CA : \n {context.user_data['coinType']}"
+    
+    reply_keyboard = [
+        [
+            InlineKeyboardButton("❌ Cancel", callback_data="cancel"),
+            InlineKeyboardButton("✅ Confirm", callback_data="confirm_purchase"),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(reply_keyboard)
+    await context.bot.edit_message_text(chat_id=front_chat_id, message_id=front_msg_id, text=text, parse_mode="HTML", reply_markup=reply_markup)
+    
+    # await context.bot.edit_message_text(
+    #     chat_id=front_chat_id,
+    #     message_id=front_msg_id,
+    #     text=f"Selected Token: \n\n CA : \n {context.user_data['coinType']}"
+    # )
+
+
     # Register the handler for token ID input
 
+async def msgHandler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    input_text = update.message.text
+    print('inputed Text', input_text)
+    if 'user_id' not in user_config:
+        user_config['user_id'] = update.message.from_user.id
+        context.user_data['user_id'] = update.message.from_user.id
+        
+        user_config['curInput'] = "coinType"
+        coinValidating = validate_coinType(input_text.strip())
+                
+        if coinValidating['val'] : 
+            user_config['curInput'] = "cointype_confirmation"
+            context.user_data['coinType'] = input_text
+            await handle_coinConfirmation(update, context)
+            
+            # context.user_data['coinType'] = input_text.strip()
+            
+    elif user_config['curInput'] == "cointype":
+        a =5
+        
+    # if val_response['val']:
+    #     context.user_data["coinType"] = coinType
+    #     await update.message.reply_text(f"Token ID received: {coinType}. Now, select the boosting period:")
+        
+    #     # Remove the token handler and proceed to the next input for boosting period
+    #     context.application.remove_handler(procedure_handler)
+        
+    #     # Display the boosting period options with buttons
+    #     await send_trending_boost_options(update, context)
+
+    #     # Register handler for boosting period input
+    #     # procedure_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, get_boosting_period)
+    #     # context.application.add_handler(procedure_handler)
+    # else:
+    #     await update.message.reply_text(f"❌ {val_response['text']}. Type 'exit' to cancel and restart.")
+    #     print(f"Invalid Token ID: {coinType}")
+        
+        
+        
+    # if input_text == "ok" and user_id not in user_config:
+    #     user_config[user_id] = "ok"
+    #     user_data[user_id] = {"first": input_text}
+    #     await update.message.reply_text(text="success")
+    # if input_text == "yes" and user_id in user_config:
+    #     text = (
+    #         f"first text: {user_data[user_id]['first']}\n"
+    #         f"second text: {input_text}"
+    #     )
+    #     await update.message.reply_text(text=text)
 
 async def main():
     global cur_coin_idx , last_txn_arr
@@ -174,12 +239,11 @@ async def main():
     application = Application.builder().token(BOT_TOKEN).read_timeout(20).write_timeout(20).build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_reply))
-    # application.add_handler(CommandHandler("start", start))
+    # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_reply))
     # application.add_handler(CommandHandler("help", help_command))
     
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, msgHandler))
     application.add_handler(CallbackQueryHandler(handle_startTrending, pattern="startTrending"))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message))
 
     # Start the polling for transactions
     asyncio.create_task(poll_transactions(application, interval=30))
